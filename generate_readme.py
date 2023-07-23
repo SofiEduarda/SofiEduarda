1e8fec80daf2cc2a9419ddf8ca016bf108c547e8
SofiEduarda <140330057+SofiEduarda@users.noreply.github.com>
2023-07-23 14:27:08 -0300

    Update generate_readme.py

--- generate_readme.py
+++ generate_readme.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 
-data_nascimento = datetime(2011, 7, 1)
+data_nascimento = datetime(2011, 4, 13)
 
 data_atual = datetime.now()
 idade = data_atual.year - data_nascimento.year - ((data_atual.month, data_atual.day) < (data_nascimento.month, data_nascimento.day))

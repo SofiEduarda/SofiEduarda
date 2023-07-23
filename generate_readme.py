7e3d0b7ba5a6eaca1350183bfd270b7cf2963a65
SofiEduarda <140330057+SofiEduarda@users.noreply.github.com>
2023-07-23 14:09:37 -0300

    Create generate_readme.py

---
+++ generate_readme.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+
+data_nascimento = datetime(2011, 7, 1)
+
+data_atual = datetime.now()
+idade = data_atual.year - data_nascimento.year - ((data_atual.month, data_atual.day) < (data_nascimento.month, data_nascimento.day))
+
+with open('README.md', 'r') as file:
+    readme_content = file.read()
+
+readme_content = readme_content.replace('{IDADE}', str(idade))
+
+with open('README.md', 'w') as file:
+    file.write(readme_content)
